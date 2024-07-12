48734feb4672a1bf924b4a738412662802fdbf6c
itajacy TI02 <itajacy@bsd.com.br>
2024-07-12 13:46:12 -0300

    ObservableFuture(2) - alteracoes na controller

--- lib/observables/future/future_controller.dart
+++ lib/observables/future/future_controller.dart
@@ -5,12 +5,19 @@ class FutureController = FutureControllerBase with _$FutureController;
 
 abstract class FutureControllerBase with Store {
   @observable
-  ObservableFuture<String> nomeFuture = Future.value('').asObservable();
+  // ObservableFuture<String> nomeFuture = Future.value('').asObservable();
+  ObservableFuture<String> nomeFuture = ObservableFuture.value('');
 
   Future<void> buscarNome() async {
-    nomeFuture =
-        Future.delayed(const Duration(seconds: 2), () => 'Rodrigo Rahman')
-            .asObservable();
+    final buscaNomeFuture =
+        Future.delayed(const Duration(seconds: 2), () => 'Rodrigo ahman');
+
+    nomeFuture = ObservableFuture(buscaNomeFuture);
+
+    // nomeFuture =
+    //     Future.delayed(const Duration(seconds: 2), () => 'Rodrigo ahman')
+    //         .asObservable();
+
     // await Future.delayed(const Duration(seconds: 2));
     // return 'Rodrigo Rahman';
   }
